f1dc139023250838433632efa2d4eb071f37c3b1
emerickschf <mac@MBP-de-mac.lan>
2023-06-26 16:48:54 +0200

    second commit

--- front/js/script.js
+++ front/js/script.js
@@ -1,27 +1,47 @@
 let meubleData = [];
 
 const fetchMeuble = async () => {
-    await fetch("http://localhost:3000/api/products")
+  await fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
     .then((promise) => {
-        meubleData = promise
-        console.log (meubleData);
-    }); 
-}; 
-
-const meubleDisplay = async () => {
-    await fetchMeuble();
-
-document.getElementById("items").innerHTML = meubleData.map(
-    (meuble) => `
-<div id="card${meuble._id}" class="items article img">
-<h3 class="items article h3">${meuble.name.toUpperCase()}</h3>
-<img class="" src="${meuble.imageUrl}" alt= "image de meuble ${meuble.name}"/>
-
-
-</div>
-`)
+      meubleData = promise;
+      console.log(meubleData);
+    });
 };
 
+const meubleDisplay = async () => {
+  await fetchMeuble();
+
+  const container = document.getElementById("items");
+  meubleData.forEach((meuble) => {
+    const card = document.createElement("div");
+    card.id = "items" + meuble._id;
+    card.className = "items article img";
+
+    const title = document.createElement("h3");
+    title.className = "items article h3";
+    title.textContent = meuble.name.toUpperCase();
+
+    const image = document.createElement("img");
+    image.className = "items article img"
+    image.src = meuble.imageUrl;
+    image.alt = "image de meuble " + meuble.name;
+
+    const description = document.createElement("p");
+    description.className = "items article p";
+    description.textContent = meuble.description;
+
+    const price = document.createElement("p");
+    price.className = "items article p";
+    price.textContent = meuble.price.toString().replace(/0+$/, "") + " Euro";
+
+    card.appendChild(title);
+    card.appendChild(image);
+    card.appendChild(description);
+    card.appendChild(price);
+
+    container.appendChild(card);
+  });
+};
 
 meubleDisplay();
